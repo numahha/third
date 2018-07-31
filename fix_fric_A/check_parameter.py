22f3c0f8d5a1843238b7295bca4f682db55ed2e8
Toru Hishinuma <numahha@gmail.com>
2018-07-31 20:43:04 +0900

    固定摩擦係数を変えた場合の最適方策の学習テスト
    うまく行かなかったやつ。一応保存。

---
+++ fix_fric_A/check_parameter.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# This code is to check parameters of the hopper task.
+# Compare the original hopper task by openAI and a slightly modified one.
+import mujoco_py # Mujoco must come before other imports. https://openai.slack.com/archives/C1H6P3R7B/p1492828680631850
+import gym
+import my_env
+import numpy
+
+
+
+#env_id = "Hopper-v2"   # the original hopper
+env_id = "MyHopper-v2"  # a modified hopper
+
+
+
+print("\n##### 0 #####")
+e = gym.make(env_id)
+print("env id is ",e.env.spec.id)
+
+
+
+print("\n##### 1 #####")
+print("e.env.model.body_mass")
+print(e.env.model.body_mass)
+#print("torso mass divided by pi is ",e.env.model.body_mass[1]/numpy.pi)
+#print(e.env.model.body_mass[1]/(numpy.pi*2.5)) # size*size*density = 0.05*0.05*1000 = 2.5
+if env_id is not "Hopper-v2":
+    def temp_torso_mass_dist():
+        return 4.0
+    e.env.torso_mass_dist = temp_torso_mass_dist
+    e.reset()
+    print("modified body_mass")
+    print(e.env.model.body_mass)
+
+
+
+print("\n##### 2 #####")
+print("e.env.model.geom_friction")
+print(e.env.model.geom_friction)
+if env_id is not "Hopper-v2":
+    def temp_friction_dist():
+        return 2.0
+    e.env.friction_dist = temp_friction_dist
+    e.reset()
+    print("modified friction")
+    print(e.env.model.geom_friction)
+
+
+
+
+e.close()
+
---
+++ fix_fric_A/job/fricfix_seed3.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+array=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0)
+
+s=3
+
+for i in ${array[@]}; do
+        python3 run_mujoco.py --env MyHopper-v2 --outdir fricfix${i}_seed${s} --num-timesteps 2000000 --fric_fix $i --seed $s
+done
+
+
+
+
+
+
+#python3 train_trpo_gym.py --env Hopper-v2 --gpu -1
